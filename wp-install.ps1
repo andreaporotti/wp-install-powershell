5a82ab42715eb2775c1dd3c450e87740b4372540
Andrea Porotti <53380085+andreaporotti@users.noreply.github.com>
2025-02-20 23:37:36 +0100

    Bump version to 1.6.3.

--- wp-install.ps1
+++ wp-install.ps1
@@ -4,7 +4,7 @@
 	---------------------------
 
 	Description:	a PowerShell script for quick Wordpress installation on local web server
-	Version:		1.6.2
+	Version:		1.6.3
 	Author:			Andrea Porotti
 	URL:			https://github.com/andreaporotti/wp-install-powershell
 
